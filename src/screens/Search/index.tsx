3e9afea3f06dc3c64f4c891e335852e2a6ba5f4a
lucaslozz <lucass.mark@gmail.com>
2023-11-14 12:51:10 -0300

    chore: create ios file and install eas update

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,17 +1,20 @@
-import { useEffect, useState } from 'react';
-import { ImageBackground, Text, View, ScrollView } from 'react-native';
+import { useEffect, useState } from "react";
+import { ImageBackground, Text, View, ScrollView } from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 import Logo from "@assets/logo.svg";
-import bg from '@assets/background.png';
+import bg from "@assets/background.png";
 
-import { useCity } from '@hooks/useCity';
-import { CityProps, getCityByNameService } from '@services/getCityByNameService';
+import { useCity } from "@hooks/useCity";
+import {
+  CityProps,
+  getCityByNameService,
+} from "@services/getCityByNameService";
 
-import { SelectList } from '@components/SelectList';
+import { SelectList } from "@components/SelectList";
 
 export function Search() {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [cities, setCities] = useState<CityProps[]>([]);
 
@@ -38,18 +41,21 @@ export function Search() {
 
   return (
     <ScrollView>
-      <ImageBackground source={bg} defaultSource={bg} style={styles.container} resizeMode="cover">
+      <ImageBackground
+        source={bg}
+        defaultSource={bg}
+        style={styles.container}
+        resizeMode="cover"
+      >
         <Logo width={186} height={32} />
 
         <View style={styles.content}>
           <Text style={styles.title}>
             Boas vindas ao <Text style={styles.brand}>iWeather</Text>
           </Text>
-
           <Text style={styles.subtitle}>
             Escolha um local para ver a previsão do tempo
           </Text>
-
           <SelectList
             data={cities}
             onChange={setSearch}
